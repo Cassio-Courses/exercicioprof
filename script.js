387e76439017e37789964efe4eaee62d2338a5ca
cassio8186 <cassio8186@gmail.com>
2019-10-18 21:25:37 -0300

    finish

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var btn = document.querySelector("btn");
+// Recebe os dados que serão utilizados no script
+var btn = document.querySelector("button");
 var input = document.querySelector("input");
-var img = document.querySelector("img");
+var txt = document.querySelector("#mensagem");
 
-// Valida o input não permitindo que passe de 12 imagens.
+// A tela iniciar atribue 12 ao valor
+window.onload = () => {
+  input.value = 12;
+};
+
+// Valida o input não permitindo que passe de 12 imagens ou menos de 0.
 input.addEventListener("focusout", function() {
   if (this.value > 12) {
     this.value = 12;
@@ -11,15 +17,33 @@ input.addEventListener("focusout", function() {
   }
 });
 /**
- * @param recebe valor do input
+ * Ao clicar no botão vai executar a função de deletar imagens
+ * E também foi adicionado validação para não permitir que input fique vazio
  */
-btn.addEventListener(function(inputValue) {});
+btn.addEventListener("click", function() {
+  console.log(input.value);
+  if (input.value === "") {
+    alert("POAMIGAO COLOCA O VALOR AI NÉ BRO");
+  } else {
+    deleteImages(input.value);
+    txt.textContent = "Atualize a aplicação para iniciar novamente :)";
+  }
+});
 
-function deleteImages(array = []) {
-  var tamanhoArray = array.length; // vai deletar o ultimo
-  if (tamanhoArray > 0) {
-    img[tamanhoArray].remove();
-    deleteImages(img - 1);
+/**
+ * Função vai deletar as imagens que o cliente não deseja que seja mantido
+ * Utilizando recursividade
+ */
+
+function deleteImages() {
+  var img = document.querySelectorAll("img");
+  var tamanhoArray = img.length;
+  // Vai deletar a quantidade de imagens que o usuário desejar
+  if (input.value < tamanhoArray) {
+    img[tamanhoArray - 1].remove();
+    // Função vai se chamar (recursividade)
+    deleteImages(input.value);
   }
 }
-deleteImages(img);
+
+
